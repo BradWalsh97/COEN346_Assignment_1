96b3633315212d64b1ed9f668211c3ebcca7d771
Brad Walsh <Bradley.walsh.97@outlook.com>
2020-01-21 17:46:26 -0500

    Read input file and create an array based off of it

--- src/com/brad/Main.java
+++ src/com/brad/Main.java
@@ -1,8 +1,28 @@
 package com.brad;
 
+import java.io.*;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        //Start by getting the input file and creating an array consisting of all the light bulbs
+        File file = new File(Paths.get("input.txt").toAbsolutePath().toString());
+        int arraySize;
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        arraySize = Integer.parseInt(in.readLine());
+        Character[] lightBulbs = new Character[arraySize];
+        System.out.println("There are " + lightBulbs.length + " light bulbs in this example.");
+        String str;
+        int lightIter = 0;
+        while ((str = in.readLine()) != null) {
+            lightBulbs[lightIter] = str.charAt(0);
+            lightIter++;
+        }
+
+        for (char c:lightBulbs) {
+            System.out.println(c);
+        }
+
     }
 }
