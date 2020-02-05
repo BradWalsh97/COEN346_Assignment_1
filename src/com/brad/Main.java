e1f3cc975c65ac5b5d8487e334e3c6a2468719d6
TheLilGrant <60147677+TheLilGrant@users.noreply.github.com>
2020-02-05 15:37:07 -0500

    Made her actually threaded

--- src/com/brad/Main.java
+++ src/com/brad/Main.java
@@ -1,15 +1,9 @@
 package com.brad;
 
-import javax.swing.text.Style;
-import java.awt.image.AreaAveragingScaleFilter;
 import java.io.*;
 import java.lang.management.ManagementFactory;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
 
 public class Main {
     public  static class MaxThreadCounter{
@@ -17,9 +11,10 @@ public class Main {
         public synchronized int getMaxThreadCounter() {return this.MaxThreadCount;}
         public synchronized void setMaxThreadCounter(int max) {this.MaxThreadCount = max;}
     }
-    private static MaxThreadCounter MaxThreads = new MaxThreadCounter();
+    protected static MaxThreadCounter MaxThreads = new MaxThreadCounter();
 
     public static void main(String[] args) throws IOException, InterruptedException {
+
         //Start by getting the input file and creating an array consisting of all the light bulbs
         File file = new File(Paths.get("input.txt").toAbsolutePath().toString());
         int arraySize;
@@ -37,12 +32,9 @@ public class Main {
         }
 
         MaxThreads.setMaxThreadCounter(0);
-        //Debug: Output light bulbs
-//        for (char c : lightBulbs)
-//            System.out.println(c);
 
         if (arraySize != lightBulbs.length) {
-            System.out.println("Array size does not match inputted array. Please try again."); //todo: input error checking
+            System.out.println("Array size does not match inputted array. Please try again.");
             return;
         } else {
             FindDefective(lightBulbs, 0, lightBulbs.length - 1);
@@ -64,12 +56,12 @@ public class Main {
             System.out.println("All 1s");
             return;
         }
-        //TODO: Try to make this global and mutex it
+
         int threadCount = ManagementFactory.getThreadMXBean().getThreadCount();
         if (threadCount > MaxThreads.getMaxThreadCounter())
             MaxThreads.setMaxThreadCounter(threadCount);
         
-        // Traverse left side & right side of the array if possible
+        // Traverse left and right side of the array
         if (pivot - startingIndex >= 1 && lastIndex - pivot >= 1) {
             Thread leftArrayThread = new Thread(new Runnable() {
                 @Override
@@ -95,10 +87,10 @@ public class Main {
             leftArrayThread.start();
             rightArrayThread.start();
             leftArrayThread.join();
-            leftArrayThread.join();
+            rightArrayThread.join();
         }
 
-        //Traverse just left side of the array
+        //Traverse the left side of the array
         else if(pivot - startingIndex >= 1){
             Thread leftArrayThread = new Thread(new Runnable() {
                 @Override
@@ -113,7 +105,7 @@ public class Main {
             leftArrayThread.start();
             leftArrayThread.join();
         }
-        // Traverse just  right side of the array
+        // Traverse the right side of the array
         else if (lastIndex - pivot >= 1) {
             Thread rightArrayThread = new Thread(new Runnable() {
                 @Override
@@ -128,12 +120,9 @@ public class Main {
             rightArrayThread.start();
             rightArrayThread.join();
         }
-
-
-
     }
 
-    // Check if the array has no defective elements
+    // Check if the array has no defective elements (Are there any zeros in the array?)
     public static boolean noDefective(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 0) {
@@ -142,89 +131,4 @@ public class Main {
         }
         return true;
     }
-    /*
-
-    private static void FindDefective(char[] lightBulbs, int arraySize) throws InterruptedException {
-        ArrayList<Integer> emptyList = new ArrayList<Integer>();
-        ArrayList<Integer> deadBulbs = FindDefective(lightBulbs, arraySize, (int) Math.ceil((double) arraySize / 2), emptyList);
-        System.out.println("There are " + deadBulbs.size() + " dead bulbs in the array.");
-        System.out.println("The dead bulbs are: ");
-        for (int i = 0; i < deadBulbs.size(); i++) {
-            if(deadBulbs.get(i) == -1)
-                continue;
-            System.out.println("Bulb " + deadBulbs.get(i));
-        }
-        System.out.println("Size: " + deadBulbs.size());
-
-    }
-
-
-
-    //return true if there is a zero present at the base array. False if there's ones.
-    private static ArrayList<Integer> FindDefective(char[] lightBulbs, int arraySize, int pivot, ArrayList<Integer> list) throws InterruptedException {
-        ArrayList<Integer> emptyList = new ArrayList<>();
-//        int[] currentReturn;
-//        int[] leftValues;
-//        int[] rightValues;
-
-        //check if array has zeros
-        boolean containsZero = false;
-        for (char c : lightBulbs) {
-            if (c == '0') {
-                containsZero = true;
-                break;
-            }
-        }
-
-        if (containsZero && arraySize >= 2) {
-            // Function to split array into two parts in Java
-            char[] leftBulbs = new char[(pivot + 1) / 2];
-            char[] rightBulbs = new char[arraySize - leftBulbs.length];
-
-            System.arraycopy(lightBulbs, 0, leftBulbs, 0, leftBulbs.length);
-            System.arraycopy(lightBulbs, leftBulbs.length, rightBulbs, 0, rightBulbs.length);
-
-//            list.addAll(FindDefective(leftBulbs, leftBulbs.length, (int) Math.ceil((double) arraySize / 2), list));
-//            list.addAll(FindDefective(rightBulbs, rightBulbs.length, (int) Math.ceil((double) arraySize / 2), list));
-            emptyList.addAll(FindDefective(leftBulbs, leftBulbs.length, (int) Math.ceil((double) arraySize / 2), list));
-            emptyList.addAll(FindDefective(rightBulbs, rightBulbs.length, (int) Math.ceil((double) arraySize / 2), list));
-            list.addAll(emptyList);
-            //list.addAll(emptyList);
-
-//            Thread leftThread = new Thread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    try {
-//                        list.addAll(FindDefective(leftBulbs, leftBulbs.length, (int) Math.ceil((double) arraySize / 2), list));
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            });
-//            Thread rightThread = new Thread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    try {
-//                        list.addAll(FindDefective(rightBulbs, rightBulbs.length, (int) Math.ceil((double) arraySize / 2), list));
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            });
-//
-//            leftThread.start();
-//            rightThread.start();
-//            leftThread.join();
-//            rightThread.join();
-        } else if (containsZero && arraySize == 1) {
-            list.add(pivot);
-            return list;
-        }
-
-        list.add(-1);
-        return list;
-    }
-    */
 }
-
-//return the pivot as position as variable
