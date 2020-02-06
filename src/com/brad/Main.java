518bc0e81f9544a8808b468d8f50e8bee30717e5
TheLilGrant <60147677+TheLilGrant@users.noreply.github.com>
2020-02-06 01:02:39 -0500

    Life is Hell, Thread count Stilly wonky
    So I was able to remove like 50 lines of code, simplified the base case. Removed the traversing only one side for a simple check if there are 0s in the array, and if so split it.

--- src/com/brad/Main.java
+++ src/com/brad/Main.java
@@ -1,16 +1,9 @@
 package com.brad;
 
 import java.io.*;
-import java.lang.management.ManagementFactory;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public class Main {
-    public  static class MaxThreadCounter{
-        public static int MaxThreadCount;
-        public synchronized int getMaxThreadCounter() {return this.MaxThreadCount;}
-        public synchronized void setMaxThreadCounter(int max) {this.MaxThreadCount = max;}
-    }
     protected static MaxThreadCounter MaxThreads = new MaxThreadCounter();
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -46,89 +39,62 @@ public class Main {
     public static void FindDefective(int[] array, int startingIndex, int lastIndex) throws InterruptedException {
         int pivot = startingIndex + (lastIndex - startingIndex) / 2;
 
-        if ((pivot == startingIndex) && array[pivot] == 0) {
-            ArrayList<Integer> positions = new ArrayList<Integer>();
-            positions.add(startingIndex);
-            positions.forEach((n) -> System.out.println("Defective bulb in position: " + n));
+        if ((lastIndex == startingIndex) && (array[pivot] == 0)) {
+                System.out.println("Defective bulb in position: " + pivot);
         }
 
-        if (noDefective(array)) {
-            System.out.println("All 1s");
+        else if (noDefective(array, startingIndex, lastIndex)) {
             return;
         }
+        
+        else {
+            Thread leftArrayThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        FindDefective(array, startingIndex, pivot);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Thread rightArrayThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        FindDefective(array, pivot + 1, lastIndex);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
-        int threadCount = ManagementFactory.getThreadMXBean().getThreadCount();
+            leftArrayThread.start();
+            rightArrayThread.start();
+            leftArrayThread.join();
+            rightArrayThread.join();
+
+        }
+
+        int threadCount = java.lang.Thread.activeCount();
         if (threadCount > MaxThreads.getMaxThreadCounter())
             MaxThreads.setMaxThreadCounter(threadCount);
-        
-        // Traverse left and right side of the array
-        if (pivot - startingIndex >= 1 && lastIndex - pivot >= 1) {
-            Thread leftArrayThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        FindDefective(array, startingIndex, pivot);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            Thread rightArrayThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        FindDefective(array, pivot + 1, lastIndex);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
 
-            leftArrayThread.start();
-            rightArrayThread.start();
-            leftArrayThread.join();
-            rightArrayThread.join();
-        }
-
-        //Traverse the left side of the array
-        else if(pivot - startingIndex >= 1){
-            Thread leftArrayThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        FindDefective(array, startingIndex, pivot);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            leftArrayThread.start();
-            leftArrayThread.join();
-        }
-        // Traverse the right side of the array
-        else if (lastIndex - pivot >= 1) {
-            Thread rightArrayThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        FindDefective(array, pivot + 1, lastIndex);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            rightArrayThread.start();
-            rightArrayThread.join();
-        }
     }
 
     // Check if the array has no defective elements (Are there any zeros in the array?)
-    public static boolean noDefective(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+    public static boolean noDefective(int[] array, int startingIndex, int lastIndex) {
+        for (int i = startingIndex; i <= lastIndex; i++) {
             if (array[i] == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    public  static class MaxThreadCounter{
+        public static int MaxThreadCount;
+        public synchronized int getMaxThreadCounter() {return this.MaxThreadCount;}
+        public synchronized void setMaxThreadCounter(int max) {this.MaxThreadCount = max;}
     }
 }
